784b39c44d158c1ea33a2ab45fbb9bfaeabce448
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2024-07-15 11:22:24 +0530

    added floyd algorithm approach to solve the problem

--- FindTheDuplicateNumber.cpp
+++ FindTheDuplicateNumber.cpp
@@ -13,4 +13,20 @@ public:
         }
         return ans;
    }
+
+   // Using Floyd Algorithm
+    int slow = nums[0];
+    int fast = nums[0];
+    do{
+       slow = nums[slow];
+       fast = nums[nums[fast]];
+    }while(slow != fast);
+
+    fast = nums[0];
+    while(slow != fast){
+       slow = nums[slow];
+       fast = nums[fast];
+    }
+    return slow;
+
 };
