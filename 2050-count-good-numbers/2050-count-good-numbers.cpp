829e981334ff40bef1bbd609414aa7c18399531b
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2024-10-02 23:09:48 +0530

    Time: 0 ms (100%), Space: 7.5 MB (80.13%) - LeetHub

---
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    const int mod = 1e9 + 7;
+    long long solve(long long x, long long n){
+        if(n == 0) return 1;
+        if(n%2 == 0){
+            return solve((x*x)%mod, n/2);
+        }
+        else{
+            return (x*solve(x, n-1))%mod;
+        }
+    }
+    int countGoodNumbers(long long n) {
+        long long even = n/2 + n % 2;
+        long long odd = n/2;
+        long long evenPos = solve(5, even);
+        long long oddPos = solve(4, odd);
+        long long ans = (evenPos*oddPos)%mod;
+        return ans;
+    }
+};
