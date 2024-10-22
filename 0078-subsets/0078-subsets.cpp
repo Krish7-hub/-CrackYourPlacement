a200ee8548d4542c9ffc00b25c84ab3b134dabcc
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2024-10-22 23:26:55 +0530

    Time: 0 ms (100%), Space: 8.6 MB (52.01%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-   void find(vector<int>&nums, int index, vector<int>output, vector<vector<int>>&ans){
-      if(index >= nums.size()){
-        ans.push_back(output);
-        return;
-      }
-      find(nums, index+1, output, ans);
-      output.push_back(nums[index]);
-      find(nums, index+1, output, ans);
-   }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<int>output;
-        int index = 0;
-        find(nums, index, output, ans);
+        int sizeNum = nums.size();
+        int subset = 1 << sizeNum;
+        for(int i = 0; i < subset; i++){
+            vector<int>temp;
+            for(int j = 0; j < sizeNum; j++){
+                if(i & (1 << j)){
+                    temp.push_back(nums[j]);
+                }
+            }
+            ans.push_back(temp);
+        }
         return ans;
     }
 };
