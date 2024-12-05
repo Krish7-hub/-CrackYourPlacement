8cb163ae3441e6813a23c4a2e863c2b2e01283f5
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2024-12-05 18:45:20 +0530

    Time: 8 ms (63.95%), Space: 44.9 MB (33.58%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -5,16 +5,14 @@ public:
         sort(g.begin(), g.end());
         int str1 = 0, str2 = 0;
         int len1 = g.size(), len2 = s.size();
-        int cnt = 0;
         while(str1 < len2 && str2 < len1){
-            if(s[str1] >= g[str2]){
-                cnt++;
-                str1++, str2++;
+
+            if(g[str2] <= s[str1]){
+                str2++;
             }
-            else if(g[str2] > s[str1]){
-                str1++;
-            }
+            str1++;
+        
         }
-        return cnt;
+        return str2;
     }
 };
