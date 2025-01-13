a5de1668abcb41193ba3890f400b0a7ec5b8ed4a
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-01-13 23:30:53 +0530

    Time: 28 ms (10.22%), Space: 76.1 MB (6.67%) - LeetHub

---
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int getIndex(vector<int>inorder, int rootElement){
+        for(int i = 0; i < inorder.size(); i++){
+            if(inorder[i] == rootElement){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int& preorderIndex, int inorderStartIndex,
+     int inorderEndIndex, int& len){
+
+        // Base Condition
+        if(preorderIndex >= len){
+            return NULL;
+        }
+        if(inorderStartIndex > inorderEndIndex){
+            return NULL;
+        }
+
+        // Step 1 -> Create the node or root of tree
+        int rootElement = preorder[preorderIndex];
+        preorderIndex++;
+        TreeNode* root = new TreeNode(rootElement);
+
+        int index = getIndex(inorder, rootElement);
+
+        // Call for left Subtree 
+        root -> left = solve(preorder, inorder, preorderIndex, inorderStartIndex, index - 1, len);
+
+        // Call for right Subtree
+        root -> right = solve(preorder, inorder, preorderIndex, index + 1, inorderEndIndex, len);
+
+        return root;
+
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+
+        int len = preorder.size();
+        int inorderStartIndex = 0;
+        int inorderEndIndex = inorder.size() - 1;
+        int preorderIndex = 0;
+        TreeNode* root = solve(preorder, inorder, preorderIndex, inorderStartIndex, inorderEndIndex, len); 
+        return root;
+    }
+};
