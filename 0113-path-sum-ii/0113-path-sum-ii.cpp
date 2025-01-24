a346769a87b686985483db244f105815d3eeb81c
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-01-24 23:07:25 +0530

    Time: 0 ms (100%), Space: 21 MB (51.21%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -11,37 +11,37 @@
  */
 class Solution {
 public:
-    
-    void solve(TreeNode* root, int& targetSum, int sum, vector<int>&path, vector<vector<int>>&ans){
+    void solve(TreeNode* root, int& targetSum, int sum, vector<int>& path, vector<vector<int>>& ans){
 
         if(root == NULL){
             return;
         }
-
-        sum = sum +  root -> val;
-        path.push_back(root->val);
-
+        int value = root -> val;
+        path.push_back(value);
+        sum += root -> val;
+        // This is the leaf node.
         if(root -> left == NULL && root -> right == NULL){
-            if(sum == targetSum){
+
+            if(targetSum == sum){
                 ans.push_back(path);
             }
+
+            // Backtrack
             path.pop_back();
             return;
+
         }
 
-        solve(root->left, targetSum, sum, path, ans);
-        solve(root->right, targetSum, sum, path, ans);
-
+        solve(root -> left, targetSum, sum, path, ans);
+        solve(root -> right, targetSum, sum, path, ans);
+        // backtrack
         path.pop_back();
-
     }
-   
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
 
-        int sum = 0;
-        vector<int>path;
         vector<vector<int>>ans;
-        solve(root, targetSum, sum, path, ans);
+        vector<int>path;
+        solve(root, targetSum, 0, path, ans);
         return ans;
         
     }
