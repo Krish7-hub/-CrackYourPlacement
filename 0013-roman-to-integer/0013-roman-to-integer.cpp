7efc90c97b8a4d85fd525f6820fc6c3aad63f746
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-02-10 19:53:30 +0530

    Time: 0 ms (100%), Space: 10 MB (86.83%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -2,24 +2,64 @@ class Solution {
 public:
     int romanToInt(string s) {
 
-        unordered_map<char, int>mpp;
-        mpp['I'] = 1;
-        mpp['V'] = 5;
-        mpp['X'] = 10;
-        mpp['L'] = 50;
-        mpp['C'] = 100;
-        mpp['D'] = 500;
-        mpp['M'] = 1000;
+        // unordered_map<char, int>mpp;
+        // mpp['I'] = 1;
+        // mpp['V'] = 5;
+        // mpp['X'] = 10;
+        // mpp['L'] = 50;
+        // mpp['C'] = 100;
+        // mpp['D'] = 500;
+        // mpp['M'] = 1000;
 
-        int ans = 0;
-        for(int i = 0; i < s.size(); i++){
-            if(mpp[s[i]] < mpp[s[i+1]]){
-                ans -= mpp[s[i]];
+        // int ans = 0;
+        // for(int i = 0; i < s.size(); i++){
+        //     if(mpp[s[i]] < mpp[s[i+1]]){
+        //         ans -= mpp[s[i]];
+        //     }
+        //     else{
+        //         ans += mpp[s[i]];
+        //     }
+        // }
+        // return ans;
+
+        int result = 0;
+        int preValue = 0;
+        for(int i = s.size() - 1; i >= 0; i--){
+            int value;
+            switch(s[i]){
+                case 'I':
+                  value = 1;
+                  break;
+                case 'V':
+                  value = 5;
+                  break;
+                case 'X':
+                  value = 10;
+                  break;
+                case 'L':
+                  value = 50;
+                  break;
+                case 'C':
+                  value = 100;
+                  break;
+                case 'D':
+                  value = 500;
+                  break;
+                case 'M':
+                  value = 1000;
+                  break;
+                default:
+                  continue;
+            }
+            if(value >= preValue){
+                result += value;
             }
             else{
-                ans += mpp[s[i]];
+                result -= value;
             }
+            preValue = value;
         }
-        return ans;
+        return result;
+
     }
 };
