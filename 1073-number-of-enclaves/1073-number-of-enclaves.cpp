ad24c98dbf0785eca2f99f90c92de86f97c90f17
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-02-21 20:50:01 +0530

    Time: 11 ms (46.03%), Space: 31.7 MB (82.21%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid, vector<vector<int>>&vis, int row, int col, int delrow[], int delcol[]){
-        vis[row][col] = 1;
-        int m = grid.size();
-        int n = grid[0].size();
-        for(int i = 0; i < 4; i++){
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-            if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && grid[nrow][ncol] == 1 && !vis[nrow][ncol]){
-                vis[nrow][ncol] = 1;
-                dfs(grid, vis, nrow, ncol, delrow, delcol);
-            }
-        }
-    }
+    // void dfs(vector<vector<int>>& grid, vector<vector<int>>&vis, int row, int col, int delrow[], int delcol[]){
+    //     vis[row][col] = 1;
+    //     int m = grid.size();
+    //     int n = grid[0].size();
+    //     for(int i = 0; i < 4; i++){
+    //         int nrow = row + delrow[i];
+    //         int ncol = col + delcol[i];
+    //         if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && grid[nrow][ncol] == 1 && !vis[nrow][ncol]){
+    //             vis[nrow][ncol] = 1;
+    //             dfs(grid, vis, nrow, ncol, delrow, delcol);
+    //         }
+    //     }
+    // }
     int numEnclaves(vector<vector<int>>& grid) {
 
         int m = grid.size();
@@ -21,24 +21,39 @@ public:
         int delcol[] = {0, +1, 0, -1};
 
         vector<vector<int>>vis(m, vector<int>(n, 0));
-
+        queue<pair<int, int>>q;
 
         // traverse first row and last row
         for(int i = 0; i < n; i++){
             if(grid[0][i] == 1 && !vis[0][i]){
-                dfs(grid, vis, 0, i, delrow, delcol);
+                q.push({0, i});
             }
             if(grid[m-1][i] == 1 && !vis[m-1][i]){
-                dfs(grid, vis, m-1, i, delrow, delcol);
+                q.push({m-1, i});
             }
         }
         // traverse first col and last col
         for(int i = 0; i < m; i++){
             if(grid[i][0] == 1 && !vis[i][0]){
-                dfs(grid, vis, i, 0, delrow, delcol);
+               q.push({i, 0});
             }
             if(grid[i][n-1] == 1 && !vis[i][n-1]){
-                dfs(grid, vis, i, n-1, delrow, delcol);
+                q.push({i, n-1});
+            }
+        }
+
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            vis[row][col] = 1;
+            for(int i = 0; i < 4; i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && grid[nrow][ncol] == 1 && !vis[nrow][ncol]){
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
             }
         }
 
