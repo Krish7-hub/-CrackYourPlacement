3371f3219553ebd6db379306ae2d02a63ca2706d
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-03-03 12:30:51 +0530

    Time: 0 ms (100%), Space: 8.8 MB (90.57%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+
+        int ans = 0;
+        int cnt = 0;
+        for(int i = 0; i < s.size(); i++){
+
+            if(s[i] != ' '){
+                cnt++;
+                ans = cnt;
+            }
+            else{
+                cnt = 0;
+            }
+        }
+        
+        return ans;
+    }
+};
