6154aabcf0d0d6f10a4e0a9ab89b793b89cb7634
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-03-19 23:47:28 +0530

    Time: 0 ms (100%), Space: 8.4 MB (9.98%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -13,9 +13,35 @@ public:
         dp[n] = solveUsingMem(n-1, dp) + solveUsingMem(n-2, dp);
         return dp[n];
     }
+
+    int solveUsingTab(int n){
+        // This is tabulation method(bottom to up approach.)
+        // step 1: Create an dp Array.
+        vector<int>dp(n+1, -1);
+
+        // step 2: Analyze base case and fill dp array accordingly.
+        dp[0] = 0;
+        if(n == 0){
+            return dp[0];
+        }
+        dp[1] = 1;
+
+        // fill remaining dp array using recursive relation.
+        // index -> 0 to n as size of array is n + 1.
+        // index of 0 is 0 and index of 1 is 1.
+        // remaining size is n - 2 i.e from 2 to n.
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        // so final ans will store in dp[n].
+        return dp[n];
+    }
+
     int fib(int n) {
         // Create dp array.
-        vector<int>dp(n+1, -1);
-        return solveUsingMem(n, dp);
+        // vector<int>dp(n+1, -1);
+        // return solveUsingMem(n, dp);
+        return solveUsingTab(n);
     }
 };
