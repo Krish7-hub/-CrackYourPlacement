255571a4d147340f4a1b0e22c2fd82878e0fbca4
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-03-21 21:22:38 +0530

    Time: 0 ms (100%), Space: 8.6 MB (17.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int solve(int n, vector<int>&dp){
+    int solveUsingMem(int n, vector<int>&dp){
+        // Base Case
         if(n == 0 || n == 1){
             return 1;
         }
+
         if(dp[n] != -1){
             return dp[n];
         }
-        int one_step = solve(n-1, dp);
-        int two_step = solve(n-2, dp);
-        dp[n] = one_step + two_step;
+
+        int ans = solveUsingMem(n - 1, dp) + solveUsingMem(n - 2, dp);
+        dp[n] = ans;
         return dp[n];
     }
     int climbStairs(int n) {
-        vector<int>dp(n + 1, -1);
-        return solve(n, dp);
+        vector<int>dp(n+1, -1);
+        return solveUsingMem(n, dp);
     }
 };
