f3afcb555ec2fd747693a4ea6642e035333bed40
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-03-30 12:49:50 +0530

    Time: 8 ms (46.29%), Space: 13.1 MB (41.8%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -21,7 +21,7 @@ public:
         }
         return ans;
     }
-
+    // Solve Using Memoization.
     int solveUsingMem(string& word1, string& word2, int i, int j, vector<vector<int>>&dp){
         // Base Case
         if(i == word1.length()){
@@ -46,14 +46,40 @@ public:
         dp[i][j] = ans;
         return ans;
     }
-
+    // Solve Using Tabulation.
+    int solveUsingTabulation(string& word1, string& word2){
+        vector<vector<int>>dp(word1.length()+1, vector<int>(word2.length()+1, -1));
+        for(int row = 0; row <= word1.length(); row++){
+            dp[row][word2.length()] =  word1.length()-row;
+        }
+        for(int col = 0; col <= word2.size(); col++){
+            dp[word1.length()][col] = word2.length()-col;
+        }
+        for(int i = word1.length()-1; i >= 0; i--){
+            for(int j = word2.length()-1; j >= 0; j--){
+                 int ans = 0;
+                 if(word1[i] == word2[j]){
+                    ans = 0 + dp[i+1][j+1];
+                }
+                else{
+                    int replace = 1 + dp[i+1][j+1];
+                    int insert = 1 + dp[i][j+1];
+                    int remove = 1 + dp[i+1][j];
+                    ans = min(replace, min(insert, remove));
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+    }
     int minDistance(string word1, string word2) {
 
         int i = 0; 
         int j = 0;
         // int ans = solveUsingRecursion(word1, word2, i, j);
-        vector<vector<int>>dp(word1.size()+1, vector<int>(word2.size()+1, -1));
-        int ans = solveUsingMem(word1, word2, i, j, dp);
+        // vector<vector<int>>dp(word1.size()+1, vector<int>(word2.size()+1, -1));
+        // int ans = solveUsingMem(word1, word2, i, j, dp);
+        int ans = solveUsingTabulation(word1, word2);
         return ans;
 
     }
