051a7626c73ea63e298726b5a51d8566168ccc91
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-03-30 10:02:22 +0530

    Time: 99 ms (11.49%), Space: 75.8 MB (72.42%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -35,14 +35,33 @@ public:
         dp[i][j] = ans;
         return ans;
     }
+    // Solve Using Tabulation.
+    int solveUsingTabulation(string& s, string& s2){
+        // Step -> 1 Create DP array.
+        vector<vector<int>>dp(s.size()+1, vector<int>(s2.size()+1, 0));
+        for(int i = s.size()-1; i >= 0; i--){
+            for(int j = s2.size()-1; j >= 0; j--){
+                int ans = 0;
+                if(s[i] == s2[j]){
+                    ans =  1 + dp[i+1][j+1];
+                }
+                else{
+                    ans =  max(dp[i][j+1], dp[i+1][j]);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+    }
     int longestPalindromeSubseq(string s) {
         string s2 = s;
         reverse(s2.begin(), s2.end());
         int i = 0;
         int j = 0;
-        vector<vector<int>>dp(s.size()+1, vector<int>(s2.size()+1, -1));
-        int ans = solveUsingMem(s, s2, i, j, dp);
+        // vector<vector<int>>dp(s.size()+1, vector<int>(s2.size()+1, -1));
+        // int ans = solveUsingMem(s, s2, i, j, dp);
         // int ans = solveUsingRecursion(s, s2, i, j);
+        int ans = solveUsingTabulation(s, s2);
         return ans;
     }
 };
