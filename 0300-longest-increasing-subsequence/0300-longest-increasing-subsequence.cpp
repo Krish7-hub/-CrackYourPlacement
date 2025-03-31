7ea3d8fadae066ecdcf32a856bec7d5b4fec12e3
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-04-01 00:37:09 +0530

    Time: 275 ms (30.07%), Space: 296.4 MB (23.78%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -32,13 +32,32 @@ public:
         dp[curr][prev+1] = ans;
         return dp[curr][prev+1];
     }
+    // Solve Using Tabulation Method
+    int solveUsingTabulation(vector<int>&nums){
+        int n = nums.size();
+        vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
+
+        for(int curr_index = nums.size()-1; curr_index >= 0; curr_index--){
+            for(int prev_index = curr_index-1; prev_index >= -1; prev_index--){
+                int include = 0;
+                if(prev_index == -1 || nums[curr_index] > nums[prev_index]){
+                    include = 1 + dp[curr_index+1][curr_index+1];
+                }
+                int exclude = dp[curr_index + 1][prev_index+1];
+                int ans = max(include, exclude);
+                dp[curr_index][prev_index+1] = ans;
+            }
+        }
+        return dp[0][0];
+    }
     int lengthOfLIS(vector<int>& nums) {
         int curr = 0;
         int prev = -1;
         // int ans = solveUsingRecursion(curr, prev, nums);
         int n = nums.size();
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-        int ans = solveUsingMem(curr, prev, nums, dp);
+        // vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        // int ans = solveUsingMem(curr, prev, nums, dp);
+        int ans = solveUsingTabulation(nums);
         return ans;
     }
 };
