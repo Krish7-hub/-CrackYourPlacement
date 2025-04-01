e5133f748b57c72748a2c076ffe50edc1ee69906
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-04-02 01:15:30 +0530

    Time: 4 ms (75.05%), Space: 119.6 MB (95.05%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -9,25 +9,36 @@ public:
     //     long long exclude = solve(ind + 1, questions);
     //     return max(include, exclude); 
     // }
-    long long solveUsingMem(long long ind, vector<long long>& dp, vector<vector<int>>& questions){
-        // Base Case
-        if(ind >= questions.size()){
-            return 0;
+    // long long solveUsingMem(long long ind, vector<long long>& dp, vector<vector<int>>& questions){
+    //     // Base Case
+    //     if(ind >= questions.size()){
+    //         return 0;
+    //     }
+    //     if(dp[ind] != -1){
+    //         return dp[ind];
+    //     }
+    //     long long include = questions[ind][0] + solveUsingMem(ind + questions[ind][1] + 1, dp, questions);
+    //     long long exclude = solveUsingMem(ind + 1, dp, questions);
+    //     dp[ind] = max(include, exclude);
+    //     return dp[ind]; 
+    // }
+    long long solveUsingTabulation(vector<vector<int>>& questions){
+        int n = questions.size();
+        vector<long long>dp(n+1, 0);
+        for(int ind = n-1; ind >= 0; ind--){
+            int point = questions[ind][0];
+            int jump = questions[ind][1];
+            int nextQuestion = min(n, jump + ind + 1);
+            dp[ind] = max(dp[ind+1], point + dp[nextQuestion]);
         }
-        if(dp[ind] != -1){
-            return dp[ind];
-        }
-        long long include = questions[ind][0] + solveUsingMem(ind + questions[ind][1] + 1, dp, questions);
-        long long exclude = solveUsingMem(ind + 1, dp, questions);
-        dp[ind] = max(include, exclude);
-        return dp[ind]; 
+        return dp[0];
     }
     long long mostPoints(vector<vector<int>>& questions) {
         long long ind = 0;
         long long  n = questions.size();
         // long long ans = solve(ind, questions);
-        vector<long long>dp(n+1, -1);
-        long long ans = solveUsingMem(ind, dp, questions);
+        // vector<long long>dp(n+1, -1);
+        long long ans = solveUsingTabulation(questions);
         return ans;
     }
 };
