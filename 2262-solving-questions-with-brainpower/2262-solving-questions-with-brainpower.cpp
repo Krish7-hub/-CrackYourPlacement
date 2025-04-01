cf967b72936c62b0711347d7482c6f6083c15882
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-04-02 00:49:55 +0530

    Time: 8 ms (49.73%), Space: 127.6 MB (16.19%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    // long long solve(int ind, vector<vector<int>>&questions){
+    //     // Base Case
+    //     if(ind >= questions.size()){
+    //         return 0;
+    //     }
+    //     long long include = questions[ind][0] + solve(ind + questions[ind][1] + 1, questions);
+    //     long long exclude = solve(ind + 1, questions);
+    //     return max(include, exclude); 
+    // }
+    long long solveUsingMem(long long ind, vector<long long>& dp, vector<vector<int>>& questions){
+        // Base Case
+        if(ind >= questions.size()){
+            return 0;
+        }
+        if(dp[ind] != -1){
+            return dp[ind];
+        }
+        long long include = questions[ind][0] + solveUsingMem(ind + questions[ind][1] + 1, dp, questions);
+        long long exclude = solveUsingMem(ind + 1, dp, questions);
+        dp[ind] = max(include, exclude);
+        return dp[ind]; 
+    }
+    long long mostPoints(vector<vector<int>>& questions) {
+        long long ind = 0;
+        long long  n = questions.size();
+        // long long ans = solve(ind, questions);
+        vector<long long>dp(n+1, -1);
+        long long ans = solveUsingMem(ind, dp, questions);
+        return ans;
+    }
+};
