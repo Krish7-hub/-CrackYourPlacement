76b448826d7099e552661f191233ba85f3233d8b
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-04-06 22:23:00 +0530

    Time: 23 ms (24.33%), Space: 13.4 MB (14.27%) - LeetHub

--- 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.cpp
@@ -18,7 +18,7 @@ public:
     //     return ans;
     // }
     long long int MOD = 1e9 + 7;
-    long long int solveUsingMem(int n, int k, int target, vector<vector<long long>>&dp){
+    int solveUsingMem(int n, int k, int target, vector<vector<int>>&dp){
         // Base Case
         if(n == 0 && target == 0){
             return 1;
@@ -32,7 +32,7 @@ public:
         if(dp[n][target] != -1){
             return dp[n][target] % MOD;
         }
-        long long int ans = 0;
+        int ans = 0;
         for(int i = 1; i <= k; i++){
             if(target - i >= 0)
                ans = (ans) % MOD + (solveUsingMem(n-1, k, target-i, dp)) % MOD;
@@ -40,10 +40,26 @@ public:
         dp[n][target] = ans % MOD;
         return dp[n][target] % MOD;
     }
+    int solveUsingTabulation(int n, int k, int target){
+        vector<vector<int>>dp(n+1, vector<int>(target+1, 0));
+        dp[0][0] = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j <= target; j++){
+                int ans = 0;
+                for(int ind = 1; ind <= k; ind++){
+                    if(j - ind >= 0)
+                    ans = (ans) % MOD + (dp[i-1][j-ind]) % MOD;
+                }
+                dp[i][j] = ans % MOD;
+            }
+        }
+        return dp[n][target];
+    }
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<long long>>dp(n+1, vector<long long>(target+1, -1));
+        vector<vector<int>>dp(n+1, vector<int>(target+1, -1));
         // int ans = solveUsingRecursion(n, k, target);
-        long long int ans = solveUsingMem(n, k, target, dp);
+        // int ans = solveUsingMem(n, k, target, dp);
+        int ans = solveUsingTabulation(n, k, target);
         return ans;
     }
 };
