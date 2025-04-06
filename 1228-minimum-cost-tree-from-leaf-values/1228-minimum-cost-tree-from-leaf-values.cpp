54b150e3ef3cb1129ad7f61a73326a142e1340fb
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-04-06 13:40:12 +0530

    Time: 30 ms (27.13%), Space: 15.3 MB (5.28%) - LeetHub

--- 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int solveUsingRecursion(int left, int right, vector<int>&arr,  map<pair<int, int>, int>&maxi, vector<vector<int>>&dp){
         // Base Case
-        // if(right == arr.size()-1){
-        //     return 0;
-        // }
         if(left == right){
             return 0;
         }
@@ -19,6 +16,21 @@ public:
         dp[left][right] = ans;
         return dp[left][right];
     }
+    int solveUsingTabulation(vector<int>&arr, map<pair<int, int>, int>&maxi){
+        // base Case analysis.
+        int n = arr.size();
+        vector<vector<int>>dp(n+1, vector<int>(n+1, 0));
+        for(int left = n-1; left >= 0; left--){
+            for(int right = 0; right < n; right++){
+                int ans = INT_MAX;
+                for(int k = left; k < right; k++){
+                    ans = min(ans, maxi[{left, k}] * maxi[{k+1, right}] + solveUsingRecursion(left, k, arr, maxi, dp) + solveUsingRecursion(k+1, right, arr, maxi, dp));
+                }
+                dp[left][right] = ans;
+            }
+        }   
+        return dp[0][n-1];
+    }
     int mctFromLeafValues(vector<int>& arr) {
         int len = arr.size();
         int left = 0;
@@ -31,7 +43,8 @@ public:
             }
         }
         vector<vector<int>>dp(len+1, vector<int>(len+1, -1));
-        int ans = solveUsingRecursion(left, right, arr, maxi, dp);
+        // int ans = solveUsingRecursion(left, right, arr, maxi, dp);
+        int ans = solveUsingTabulation(arr, maxi);
         return ans;
         
     }
