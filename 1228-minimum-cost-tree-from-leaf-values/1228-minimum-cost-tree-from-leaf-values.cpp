83f81577df45ad5ba819940519bd187245c74d81
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-04-06 13:11:03 +0530

    Time: 25 ms (43.96%), Space: 14.9 MB (13.52%) - LeetHub

--- 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
-    int maxVal(int start, int end, vector<int>&arr){
-        int maxi = INT_MIN;
-        for(int i = start; i <= end; i++){
-            if(arr[i] > maxi){
-                maxi = arr[i];
-            }
-        }
-        return maxi;
-    }
-    int solveUsingRecursion(int left, int right, vector<int>&arr, vector<vector<int>>&dp){
+    int solveUsingRecursion(int left, int right, vector<int>&arr,  map<pair<int, int>, int>&maxi, vector<vector<int>>&dp){
         // Base Case
         // if(right == arr.size()-1){
         //     return 0;
@@ -23,7 +14,7 @@ public:
         // Processing
         int ans = INT_MAX;
         for(int k = left; k < right; k++){
-            ans = min(ans, maxVal(left, k, arr) * maxVal(k+1, right, arr) + solveUsingRecursion(left, k, arr, dp) + solveUsingRecursion(k+1, right, arr, dp));
+            ans = min(ans, maxi[{left, k}] * maxi[{k+1, right}] + solveUsingRecursion(left, k, arr, maxi, dp) + solveUsingRecursion(k+1, right, arr, maxi, dp));
         }
         dp[left][right] = ans;
         return dp[left][right];
@@ -32,8 +23,15 @@ public:
         int len = arr.size();
         int left = 0;
         int right = len - 1;
+        map<pair<int, int>, int>maxi;
+        for(int i = 0; i < len; i++){
+            maxi[{i, i}] = arr[i];
+            for(int j = i+1; j < len; j++){
+                maxi[{i, j}] = max(arr[j], maxi[{i, j-1}]);
+            }
+        }
         vector<vector<int>>dp(len+1, vector<int>(len+1, -1));
-        int ans = solveUsingRecursion(left, right, arr, dp);
+        int ans = solveUsingRecursion(left, right, arr, maxi, dp);
         return ans;
         
     }
