780f5a6ad82d3803e466d8d71bf7aa675a5870cd
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-04-08 00:37:28 +0530

    Time: 181 ms (16.33%), Space: 101.2 MB (17.78%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,88 +1,52 @@
 class Solution {
 public:
-    // bool solve(int currSum, int& target, int ind, vector<int>&nums){
+    // bool solveUsingRecursion(int ind, int currSum, int& target, vector<int>&nums){
+    //     // Base Case
     //     if(ind >= nums.size()){
+    //         return false;
+    //     }
+    //     if(currSum > target){
     //         return false;
     //     }
     //     if(currSum == target){
     //         return true;
     //     }
-    //     bool inc =  solve(currSum + nums[ind], target, ind+1, nums);
-    //     bool exc =  solve(currSum, target, ind+1, nums);
+    //     bool inc = solveUsingRecursion(ind+1, currSum+nums[ind], target, nums);
+    //     bool exc = solveUsingRecursion(ind+1, currSum, target, nums);
     //     return (inc || exc);
     // }
-    bool solveUsingMem(int currSum, int& target, int ind, vector<int>&nums, vector<vector<int>>&dp){
+    bool solveUsingMem(int ind, int currSum, int& target, vector<int>&nums, vector<vector<int>>&dp){
+        // Base Case
         if(ind >= nums.size()){
-            return false;
-        }
-        if(currSum == target){
-            return true;
+            return 0;
         }
         if(currSum > target){
             return 0;
         }
+        if(currSum == target){
+            return 1;
+        }
         if(dp[ind][currSum] != -1){
             return dp[ind][currSum];
         }
-        bool inc =  solveUsingMem(currSum + nums[ind], target, ind+1, nums, dp);
-        bool exc =  solveUsingMem(currSum, target, ind+1, nums, dp);
-        return dp[ind][currSum] = (inc || exc);
-    }
-    bool solveUsingTabulation(vector<int>& nums, int& target){
-        int n = nums.size();
-        vector<vector<int>>dp(n+1, vector<int>(target+1, 0));
-        for(int row = 0; row <= n; row++){
-            dp[row][target] = 1;
-        }
-        for(int ind = n-1; ind >= 0; ind--){
-            for(int s = target; s >= 0; s--){
-                bool inc = 0;
-                if(s + nums[ind] <= target){
-                    inc =  dp[ind+1][s + nums[ind]];
-                }
-                bool exc =  dp[ind+1][s];
-                dp[ind][s] = (inc || exc);
-            }
-        }
-        return dp[0][0];
-    }
-    bool solveUsingTabulationSO(vector<int>& nums, int& target){
-        int n = nums.size();
-        // vector<vector<int>>dp(n+1, vector<int>(target+1, 0));
-        vector<int>curr(target+1, 0);
-        vector<int>next(target+1, 0);
-
-        curr[target] = 1;
-        next[target] = 1;
-
-        for(int ind = n-1; ind >= 0; ind--){
-            for(int s = target; s >= 0; s--){
-                bool inc = 0;
-                if(s + nums[ind] <= target){
-                    inc =  next[s + nums[ind]];
-                }
-                bool exc =  next[s];
-                curr[s] = (inc || exc);
-            }
-            next = curr;
-        }
-        return curr[0];
+        bool inc = solveUsingMem(ind+1, currSum+nums[ind], target, nums, dp);
+        bool exc = solveUsingMem(ind+1, currSum, target, nums, dp);
+        dp[ind][currSum] = (inc || exc);
+        return dp[ind][currSum];
     }
     bool canPartition(vector<int>& nums) {
+        int n = nums.size();
         int sum = 0;
         for(int i = 0; i < nums.size(); i++){
             sum += nums[i];
         }
-        if(sum % 2 != 0){
-            return false;
-        }
+        if(sum & 1) return false;
+        int currSum = 0;
         int ind = 0;
-        int target = sum / 2;
-        // int currSum = 0;
-        // bool ans = solve(currSum, target, ind, nums);
-        // vector<vector<int>>dp(nums.size(), vector<int>(target, -1));
-        // bool ans = solveUsingMem(currSum, target, ind, nums, dp);
-        bool ans = solveUsingTabulationSO(nums, target);
+        int target = sum/2;
+        // bool ans = solveUsingRecursion(ind, currSum, target, nums);
+        vector<vector<int>>dp(n+1, vector<int>(target+1, -1));
+        bool ans = solveUsingMem(ind, currSum, target, nums, dp);
         return ans;
     }
 };
