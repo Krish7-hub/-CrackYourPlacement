463455be01fb3202a99be8c8bceb524ec8249149
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-04-09 00:50:40 +0530

    Time: 0 ms (100%), Space: 17.6 MB (71.8%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,26 +1,48 @@
 class Solution {
 public:
-    int solveUsingRecursion(int ind, int& stair, vector<int>&cost, vector<int>&dp){
-        // Base Case
-        if(ind >= cost.size()){
-            return 0;
+    // int solveUsingRecursion(int stair, vector<int>&cost){
+    //     // Base Case
+    //     if(stair == 0){
+    //         return cost[0];
+    //     }
+    //     if(stair == 1){
+    //         return cost[1];
+    //     }
+    //     int ans = cost[stair] + min(solveUsingRecursion(stair - 1, cost), solveUsingRecursion(stair-2, cost));
+    //     return ans;
+    // }
+    // int solveUsingMem(int stair, vector<int>&cost, vector<int>&dp){
+    //     // base case
+    //     if(stair == 0){
+    //         return cost[0];
+    //     }
+    //     if(stair == 1){
+    //         return cost[1];
+    //     }
+    //     if(dp[stair] != -1){
+    //         return dp[stair];
+    //     }
+    //     int ans = cost[stair] + min(solveUsingMem(stair - 1, cost, dp), solveUsingMem(stair-2, cost, dp));
+    //     dp[stair] = ans;
+    //     return dp[stair];
+    // }
+    int solveUsingTabulation(vector<int>&cost){
+        int n = cost.size();
+        vector<int>dp(n, 0);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i = 2; i < n; i++){
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
         }
-        if(dp[ind] != -1){
-            return dp[ind];
-        }
-        int ans = INT_MAX;
-        int jumpOne = cost[ind] +  solveUsingRecursion(ind+1, stair, cost, dp);
-        int jumpTwo = cost[ind] +  solveUsingRecursion(ind+2, stair, cost, dp);
-        ans = min(jumpOne, jumpTwo);
-        dp[ind] = ans;
-        return dp[ind];
+        return min(dp[n-1], dp[n-2]);
     }
     int minCostClimbingStairs(vector<int>& cost) {
-
-        int stair = cost.size();
-        vector<int>dp(stair, -1);
-        int ans1 = solveUsingRecursion(0, stair, cost, dp);
-        int ans2 = solveUsingRecursion(1, stair, cost, dp);
-        return min(ans1, ans2);
+        // int stair = cost.size();
+        // vector<int>dp(stair, -1);
+        // int ans = min(solveUsingRecursion(stair - 1, cost), solveUsingRecursion(stair-2, cost));
+        // Step -> 1
+        // int ans = min(solveUsingMem(stair - 1, cost, dp), solveUsingMem(stair-2, cost, dp));
+        int ans = solveUsingTabulation(cost);
+        return ans;
     }
 };
