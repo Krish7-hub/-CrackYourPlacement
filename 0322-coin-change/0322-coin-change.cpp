381a0e1b30a8a022a93e694519cbf54f4e46e45a
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-04-10 00:44:26 +0530

    Time: 22 ms (69.51%), Space: 22.1 MB (44.9%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -40,11 +40,29 @@ public:
         dp[target] = mini;
         return dp[target];
     }
+    int solveUsingTabulation(vector<int>&coins, int amount){
+        vector<int>dp(amount+1, 0);
+        for(int i = 1; i <= amount; i++){
+            int mini = INT_MAX;
+            for(int j = 0; j < coins.size(); j++){
+                if(i - coins[j] >= 0){
+                    int recursionAns = dp[i - coins[j]];
+                    if(recursionAns != INT_MAX){
+                        int ans = 1 + recursionAns;
+                        mini = min(ans, mini);
+                    }
+                }
+            }
+            dp[i] = mini;
+        }
+        return dp[amount];
+    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         vector<int>dp(amount+1, -1);
         // int ans = solveUsingRecursion(ind, coins, amount);
-        int ans = solveUsingMem(coins, amount, dp);
+        // int ans = solveUsingMem(coins, amount, dp);
+        int ans = solveUsingTabulation(coins, amount);
         if(ans != INT_MAX)  return ans; 
         return -1;
     }
