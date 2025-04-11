0d5e7f1f3a003527b58d53d8083404d1418724fa
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-04-11 13:52:01 +0530

    Time: 0 ms (100%), Space: 10.5 MB (69.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -22,12 +22,23 @@ public:
         dp[ind] = max(inc, exc);
         return dp[ind];
     }
+    int solveUsingTabulation(vector<int>&nums){
+        int n = nums.size();
+        vector<int>dp(n+2, 0);
+        for(int ind = n-1; ind >= 0; ind--){
+            int inc = nums[ind] + dp[ind+2];
+            int exc = dp[ind+1];
+            dp[ind] = max(inc, exc);
+        }
+        return dp[0];
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
         int ind = 0;
         // int ans = solveUsingRecursion(ind, nums);
-        vector<int>dp(n, -1);
-        int ans = solveUsingMem(ind, nums, dp);
+        // vector<int>dp(n, -1);
+        // int ans = solveUsingMem(ind, nums, dp);
+        int ans = solveUsingTabulation(nums);
         return ans;
     }
 };
