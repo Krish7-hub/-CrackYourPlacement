635967e4fbf5aa944aa41389a3703653e70cdf2a
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-04-13 12:16:17 +0530

    Time: 0 ms (100%), Space: 10.2 MB (89.27%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -39,22 +39,38 @@ public:
         return max(dp1[0], dp2[0]);
     }
 
+    int solveUsingTabulationSO(vector<int>&arr){
+        int prev = 0;
+        int prev2 = 0;
+        for(int i = arr.size()-1; i >= 0; i--){
+            int inc = arr[i] + prev2;
+            int exc = prev;
+            int curr = max(inc, exc);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+
     int rob(vector<int>& nums) {
         // int ind = 0;
         int n = nums.size();
         if(n == 1) return nums[0];
         // vector<int>dp1(n-1, -1), dp2(n-1, -1);
-        // vector<int>arr1, arr2;
-        // for(int i = 0; i < nums.size(); i++){
-        //     if(i != n-1){
-        //         arr1.push_back(nums[i]);
-        //     }
-        //     if(i != 0){
-        //         arr2.push_back(nums[i]);
-        //     }
-        // }
+        vector<int>arr1, arr2;
+        for(int i = 0; i < nums.size(); i++){
+            if(i != n-1){
+                arr1.push_back(nums[i]);
+            }
+            if(i != 0){
+                arr2.push_back(nums[i]);
+            }
+        }
+        int ans = max(solveUsingTabulationSO(arr1) , solveUsingTabulationSO(arr2));
+        return ans;
         // int ans1 = solveUsingTabulation(arr1);
         // int ans2 = solveUsingTabulation(arr2);
-        return solveUsingTabulation(nums);
+        // return solveUsingTabulation(nums);
+
     }
 };
