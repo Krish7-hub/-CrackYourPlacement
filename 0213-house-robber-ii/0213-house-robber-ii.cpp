9ee14417e2777d6082b99f57f92bce2645b20e71
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-04-13 12:00:31 +0530

    Time: 3 ms (7.98%), Space: 10.9 MB (40.44%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solveUsingRecursion(int ind, vector<int>&arr, vector<int>&dp){
+    int solveUsingMem(int ind, vector<int>&arr, vector<int>&dp){
         // Base Case
         if(ind >= arr.size()){
             return 0;
@@ -8,18 +8,16 @@ public:
         if(dp[ind] != -1){
             return dp[ind];
         }
-        int inc = arr[ind] + solveUsingRecursion(ind+2, arr, dp);
-        int exc = solveUsingRecursion(ind+1, arr, dp);
+        int inc = arr[ind] + solveUsingMem(ind+2, arr, dp);
+        int exc = solveUsingMem(ind+1, arr, dp);
         dp[ind] = max(inc, exc);
         return dp[ind];
     }
 
-    int rob(vector<int>& nums) {
-        int ind = 0;
+    int solveUsingTabulation(vector<int>&nums){
         int n = nums.size();
-        if(n == 1) return nums[0];
-        vector<int>dp1(n-1, -1), dp2(n-1, -1);
-        vector<int>arr1, arr2;
+        vector<int>arr1,arr2;
+        vector<int>dp1(n+2, 0), dp2(n+2,0);
         for(int i = 0; i < nums.size(); i++){
             if(i != n-1){
                 arr1.push_back(nums[i]);
@@ -28,8 +26,35 @@ public:
                 arr2.push_back(nums[i]);
             }
         }
-        int ans1 = solveUsingRecursion(ind, arr1, dp1);
-        int ans2 = solveUsingRecursion(ind, arr2, dp2);
-        return max(ans1, ans2);
+        for(int i = n-2; i >= 0; i--){
+            int inc = arr1[i] + dp1[i+2];
+            int exc = dp1[i+1];
+            dp1[i] = max(inc, exc);
+        }
+        for(int i = n-2; i >= 0; i--){
+            int inc = arr2[i] + dp2[i+2];
+            int exc = dp2[i+1];
+            dp2[i] = max(inc, exc);
+        }
+        return max(dp1[0], dp2[0]);
+    }
+
+    int rob(vector<int>& nums) {
+        // int ind = 0;
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        // vector<int>dp1(n-1, -1), dp2(n-1, -1);
+        // vector<int>arr1, arr2;
+        // for(int i = 0; i < nums.size(); i++){
+        //     if(i != n-1){
+        //         arr1.push_back(nums[i]);
+        //     }
+        //     if(i != 0){
+        //         arr2.push_back(nums[i]);
+        //     }
+        // }
+        // int ans1 = solveUsingTabulation(arr1);
+        // int ans2 = solveUsingTabulation(arr2);
+        return solveUsingTabulation(nums);
     }
 };
