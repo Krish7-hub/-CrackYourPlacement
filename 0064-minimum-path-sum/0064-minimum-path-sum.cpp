2dc20659c4051b49ea2d9142228f9a697e127d55
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-04-22 00:10:47 +0530

    Time: 3 ms (29.8%), Space: 16.4 MB (38.26%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -2,25 +2,30 @@ class Solution {
 public:
     int solveRecursion(int i, int j, int m, int n, vector<vector<int>>&grid, vector<vector<int>>&dp){
         // Base Case 
-        if(i >= m || j >= n){
+        if(i < 0 || j < 0){
             return 100000;
         }
-        if(i == (m-1) && j == (n-1)){
-            return grid[i][j];
+        if(i == 0 && j == 0){
+            return grid[0][0];
         }
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        int rightMove = grid[i][j] + solveRecursion(i, j+1, m, n, grid, dp);
-        int downMove = grid[i][j] + solveRecursion(i+1, j, m, n, grid, dp);
-        dp[i][j] = min(rightMove, downMove);
+        int leftMove = grid[i][j] + solveRecursion(i, j-1, m, n, grid, dp);
+        int upMove = grid[i][j] + solveRecursion(i-1, j, m, n, grid, dp);
+        dp[i][j] = min(leftMove, upMove);
         return dp[i][j];
     }
+    // int solveTabulation(vector<vector<int>>&grid){
+    //     int m = grid.size();
+    //     int n = grid[0].size();
+    //     vector<vector<int>>dp(m, vector<int>(n, -1));
+    // }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>>dp(m, vector<int>(n, -1));
-        int ans = solveRecursion(0, 0, m, n, grid, dp);
+        int ans = solveRecursion(m-1, n-1, m, n, grid, dp);
         return ans;
     }
 };
