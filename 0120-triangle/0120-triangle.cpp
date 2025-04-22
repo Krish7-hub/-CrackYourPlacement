cdd7c8561e7001dd7c8e4499ac1b46329187eb79
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-04-22 16:17:07 +0530

    Time: 0 ms (100%), Space: 13.4 MB (5.44%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -22,12 +22,32 @@ public:
         dp[i][j] = min(same, another);
         return dp[i][j];
     }
+    int solveTab(vector<vector<int>>& triangle){
+
+        int m = triangle.size();
+        int n = triangle[m-1].size();
+
+        vector<vector<int>>dp(m, vector<int>(n, -1));
+        for(int i = 0; i < n; i++){
+            dp[m-1][i] = triangle[m-1][i];
+        }
+
+        for(int i = m-2; i >= 0; i--){
+            for(int j = 0; j < triangle[i].size(); j++){
+                int same = triangle[i][j] + dp[i+1][j];
+                int another = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(same, another);
+            }
+        }
+        return dp[0][0];
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
         int n = triangle[m-1].size();
         vector<vector<int>>dp(m, vector<int>(n, -1));
         // int ans = solveRecursion(0, 0, m, n, triangle);
-        int ans = solveMem(0, 0, m, n, triangle, dp);
+        // int ans = solveMem(0, 0, m, n, triangle, dp);
+        int ans = solveTab(triangle);
         return ans;
     }
 };
