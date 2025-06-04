7a8ac4f07714be644d2d01002d13cfc2ecdcba91
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-06-04 23:48:46 +0530

    Time: 0 ms (100%), Space: 9.3 MB (56.8%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,71 +1,23 @@
 class Solution {
 public:
-    int solve(int row, int col, int& m, int& n){
-        // Base case
-        if(row >= m || col >= n){
+    int solve(int down, int right, int m, int n, vector<vector<int>>&dp){
+        // Base Case
+        if(down >= m || right >= n){
             return 0;
         }
-        if(row == (m-1) && col == (n-1)){
+        if(down == (m-1) && right == (n-1)){
             return 1;
         }
-        int downMove = solve(row+1, col, m, n);
-        int rightMove = solve(row, col+1, m, n);
-        return (downMove + rightMove);
-    }
-    int solveMem(int row, int col, int& m, int& n, vector<vector<int>>&dp){
-        // Base case
-        if(row >= m || col >= n){
-            return 0;
+        if(dp[down][right] != -1){
+            return dp[down][right];
         }
-        if(row == (m-1) && col == (n-1)){
-            return 1;
-        }
-        if(dp[row][col] != -1){
-            return dp[row][col];
-        }
-        int downMove = solveMem(row+1, col, m, n, dp);
-        int rightMove = solveMem(row, col+1, m, n, dp);
-        dp[row][col] = (downMove + rightMove);
-        return dp[row][col];
-    }
-    int solveTab(int m, int n){
-        vector<vector<int>>dp(m+1, vector<int>(n+1, 0));
-        for(int i = m-1; i >= 0; i--){
-            for(int j = n-1; j >= 0; j--){
-                if(i == m-1 && j == n-1){
-                    dp[i][j] = 1;
-                }
-                else{
-                    int downMove = dp[i+1][j];
-                    int rightMove = dp[i][j+1];
-                    dp[i][j] = downMove + rightMove;
-                }
-            }
-        }
-        return dp[0][0];
-    }
-    int solveTabSO(int m, int n){
-        vector<int>curr(n+1, 0);
-        vector<int>next(n+1, 0); 
-        for(int i = m-1; i >= 0; i--){
-            for(int j = n-1; j >= 0; j--){
-                if(i == m-1 && j == n-1){
-                    curr[j] = 1;
-                }
-                else{
-                    int downMove =  next[j];
-                    int rightMove = curr[j+1];
-                    curr[j] = downMove + rightMove;
-                }
-            }
-            next = curr;
-        }
-        return next[0];
+        int downMove = solve(down+1, right, m, n, dp);
+        int rightMove = solve(down, right+1, m, n, dp);
+        return dp[down][right] = (downMove + rightMove);
     }
     int uniquePaths(int m, int n) {
-        // return solve(0, 0, m, n);
-        // vector<vector<int>>dp(m, vector<int>(n, -1));
-        // return solveMem(0, 0, m, n, dp);
-        return solveTabSO(m, n);
+        vector<vector<int>>dp(m, vector<int>(n, -1));
+        return solve(0, 0, m, n, dp);
+        
     }
 };
