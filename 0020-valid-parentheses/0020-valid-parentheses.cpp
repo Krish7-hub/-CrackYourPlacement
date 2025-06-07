6c2de43250332fc12092003d76309c6a3197131e
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-06-07 21:18:38 +0530

    Time: 0 ms (100%), Space: 8.5 MB (96.36%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
+    bool isMatch(char c1, char c2){
+        if(c1 == '(' && c2 == ')') return true;
+        if(c1 == '{' && c2 == '}') return true;
+        if(c1 == '[' && c2 == ']') return true;
+        return false;
+    }
     bool isValid(string s) {
 
         int n = s.size();
-        stack<char>st;
+        int top = -1;
         for(int i = 0; i < n; i++){
-            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
-                st.push(s[i]);
+            if(top < 0 || !isMatch(s[top], s[i])){
+                top++;
+                s[top] = s[i];
             }
             else{
-                if(st.empty()){
-                    return false;
-                }
-                else if((s[i] == ')' && st.top() == '(') || (s[i] == '}' && st.top() == '{') || (s[i] == ']' && st.top() == '[')){
-                    st.pop();
-                }
-                else{
-                    return false;
-                }
+                top--;
             }
         }
-        return st.empty();
+        return top == -1;
     }
 };
