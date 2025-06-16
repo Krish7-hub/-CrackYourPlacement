033b5c7ccdf028e2907763d1343cc918ce295b3a
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-06-17 00:42:51 +0530

    Time: 4 ms (17.11%), Space: 10 MB (58.06%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string check = "";
-        for(int i = 0; i < s.size(); i++){
-            if(s[i]>='A' && s[i] <= 'Z'){
-                check += s[i] - 'A' + 'a' ;
-            }
-            else if((s[i] >= 'a' && s[i] <= 'z')){
-                check += s[i];
-            }
-            else if(s[i] >= '0' && s[i] <= '9'){
-                check += s[i];
-            }
-        }
         int low = 0;
-        int high = check.size() - 1;
+        int high = s.size() - 1;
         while(low <= high){
-            if(check[low] != check[high]){
+            if(!isalnum(s[low])){
+                low++;
+                continue;
+            }
+            if(!isalnum(s[high])){
+                high--;
+                continue;
+            }
+            if(tolower(s[low]) != tolower(s[high])){
                 return false;
             }
             else{
