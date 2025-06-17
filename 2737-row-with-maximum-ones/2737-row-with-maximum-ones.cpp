67e319c55c7dd3225e7de37452cb9230c39626d2
KRISH KUMAR <117804938+Krish7-hub@users.noreply.github.com>
2025-06-17 11:05:00 +0530

    Time: 7 ms (32.01%), Space: 70.3 MB (5.95%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
+    int lb(vector<int>vec, int n){
+        sort(vec.begin(), vec.end());
+        int low = 0;
+        int high = n - 1;
+        int ans = n;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(vec[mid] >= 1){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
 
         int m = mat.size();
         int n = mat[0].size();
-        int ind = 0, val = 0;
+        int maxind = 0, maxval = 0;
         for(int i = 0; i < m; i++){
-            int maxval = 0;
-            for(int j = 0; j < n; j++){
-                if(mat[i][j] == 1){
-                    maxval++;
-                }
-            }
-            if(maxval > val){
-                val = maxval;
-                ind = i;
+            int val = n - lb(mat[i], n);
+            if(val > maxval){
+                maxval = val;
+                maxind = i;
             }
         }
-        return {ind, val};
+        return {maxind, maxval};
         
     }
 };
